1dd1d61c129efb2a5bceb366d153489c62fa8cb1
农民也很疯狂 <qluoluo@icloud.com>
2015-12-16 17:03:11 -0600

    time out today
    It adding some table view next step is webkit handle javascript call
    back

---
+++ p4a-smart/TextPrinterDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  PrinterInfoModel.swift
+//  publish4AllUtilityTools
+//
+//  Created by ZhuWeicheng on 9/21/15.
+//  Copyright © 2015 Zhu,Weicheng. All rights reserved.
+//
+
+import RealmSwift
+import Foundation
+
+class TextPrinterData: Object {
+    
+    dynamic var name = ""
+    dynamic var pageNumber = ""
+    
+}
--- p4a-smart/ViewController.swift
+++ p4a-smart/ViewController.swift
@@ -7,21 +7,94 @@
 //
 
 import Cocoa
+import Realm
+import Foundation
+import RealmSwift
+import WebKit
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate, WKScriptMessageHandler, WebPolicyDelegate, WebFrameLoadDelegate, WebUIDelegate  {
+    
+    
+    //view outlet
+    @IBOutlet weak var webView: WebView!
+    @IBOutlet weak var tableView: NSTableView!
+    
+    
+    //realm Data configuration
+    let realm = try! Realm()
+    
+    
+        //realm data variables
+        let textPrinterData = TextPrinterData()
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        self.tableView.reloadData()
+        NSUserDefaults.standardUserDefaults().setBool(true, forKey: "WebKitDeveloperExtras")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        webView.policyDelegate = self;
+        webView.frameLoadDelegate = self;
+        webView.UIDelegate = self;
+        webView.editingDelegate = self;
+        
 
+        
+        
+        let try6 = NSURL(fileURLWithPath:NSBundle.mainBundle().pathForResource("index", ofType:"html")!)
+        let fragUrl = NSURL(string: "index.html", relativeToURL: try6)!
+        let request = NSURLRequest(URL: fragUrl)
+        self.webView.mainFrame.loadRequest(request)
+        
         // Do any additional setup after loading the view.
     }
+    
 
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+func numberOfRowsInTableView(tableView: NSTableView) -> Int
+{
+    return 1
+}
 
+func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView?
+{
+    let cellView = tableView.makeViewWithIdentifier("cell", owner: self) as! NSTableCellView
+    
+    cellView.textField!.stringValue = "sds"
+    
+    return cellView
+}
+
+func tableViewSelectionDidChange(notification: NSNotification)
+{
+    if (self.tableView.numberOfSelectedRows > 0)
+    {
+        let selectedItem = "sds"
+        
+   print(selectedItem)
+        
+    }
+    
+}
+
+//    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage){
+//        if(message.name == "callbackHandler") {
+//            print("JavaScript is sending a message \(message.body)")
+//        }
+//    }
+    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+        if(message.name == "callbackHandler") {
+            print("JavaScript is sending a message \(message.body)")
+        }
+    }
 
 }
 
