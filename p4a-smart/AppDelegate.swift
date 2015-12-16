b3f0052d2f96c46dc3d31969dd4240d71ee06837
农民也很疯狂 <qluoluo@icloud.com>
2015-12-16 14:05:48 -0600

    Initial Commit

---
+++ p4a-smart/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  p4a-smart
+//
+//  Created by Weicheng Zhu on 12/16/15.
+//  Copyright © 2015 Weicheng Zhu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ p4a-smart/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  p4a-smart
+//
+//  Created by Weicheng Zhu on 12/16/15.
+//  Copyright © 2015 Weicheng Zhu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
